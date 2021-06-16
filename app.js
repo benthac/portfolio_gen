d7225d0b0759a3269fccf8da20b4cdfd9e2f6303
ben thacker <benthac@gmail.com>
2021-06-16 18:32:15 -0400

    edits

--- app.js
+++ app.js
@@ -1,15 +1,71 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+// inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'what is your name?'
+//         }
+//     ])
+//     .then(answers => console.log(answers));
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'what is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'provide some information about yourself:'
+        }
+    ]);
+};
+
+
+const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+    }
+    console.log(``);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is the name of your project?'
+        },
+        {
+            type: 'checkbox',
+            name: 'link',
+            message: 'enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'would you like to enter another project?'
+            default: false
         }
     ])
-    .then(answers => console.log(answers));
-
+}
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
 // const fs = require('fs');
 
 // const generatePage = require('./src/page-template.js');
